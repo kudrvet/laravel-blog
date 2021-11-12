ff0774a9a48264f88b903fc4e9565fdccecbd498
Vitaliy <kudrvet@gmail.com>
2021-11-12 11:07:51 +0300

    - set updateArticllesLikesAndViews daily - set all defaults in .env.example

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new UpdateArticlesLikesAndViewsJob())->everyMinute();
+        $schedule->job(new UpdateArticlesLikesAndViewsJob())->daily();
     }
 
     /**
--- setup.sh
+++ setup.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
-#  docker run --rm \
-#    -u "$(id -u):$(id -g)" \
-#    -v $(pwd):/var/www/html \
-#    -w /var/www/html \
-#    laravelsail/php80-composer:latest \
-#    composer install --ignore-platform-reqs --no-scripts;
-
-	docker compose up -d --build
-#	docker exec -it laravel-blog-laravel.test-1 make install
+  cp .env.example .env &&
+	docker compose up
